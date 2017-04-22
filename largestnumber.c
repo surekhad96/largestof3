823ac32072c72211b3ce195a2de77159549074be
surekhad96 <surekhasugan96@gmail.com>
2017-04-22 14:50:25 +0530

    Update largestnumber.c

--- largestnumber.c
+++ largestnumber.c
@@ -4,3 +4,18 @@ int main()
 {
 int a,b,c;
 scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+{
+  printf("\n %d is the largest number,a");
+}
+  else if((b>a)&&(b>c))
+  {
+    printf("\n %d is the largest number,b");
+  }
+  else
+  {
+    printf("%d is the largest,c");
+  }
+  return 0;
+  getch();
+}
